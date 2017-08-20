75c7683fc31d1a8dfae6cde2444a3263dfdbb644
tamal <tamal@appscode.com>
2017-08-20 14:26:02 -0700

    Add ConfigMap

---
+++ configmap.go
@@ -0,0 +1,78 @@
+package kutil
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/mattbaird/jsonpatch"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	clientset "k8s.io/client-go/kubernetes"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
+)
+
+func CreateOrPatchConfigMap(c clientset.Interface, obj *apiv1.ConfigMap) (*apiv1.ConfigMap, error) {
+	cur, err := c.CoreV1().ConfigMaps(obj.Namespace).Get(obj.Name, metav1.GetOptions{})
+	if kerr.IsNotFound(err) {
+		return c.CoreV1().ConfigMaps(obj.Namespace).Create(obj)
+	} else if err != nil {
+		return nil, err
+	}
+	return PatchConfigMap(c, cur, func(*apiv1.ConfigMap) *apiv1.ConfigMap { return obj })
+}
+
+func PatchConfigMap(c clientset.Interface, cur *apiv1.ConfigMap, transform func(*apiv1.ConfigMap) *apiv1.ConfigMap) (*apiv1.ConfigMap, error) {
+	curJson, err := json.Marshal(cur)
+	if err != nil {
+		return nil, err
+	}
+
+	modJson, err := json.Marshal(transform(cur))
+	if err != nil {
+		return nil, err
+	}
+
+	patch, err := jsonpatch.CreatePatch(curJson, modJson)
+	if err != nil {
+		return nil, err
+	}
+	pb, err := json.MarshalIndent(patch, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+	glog.V(5).Infof("Patching ConfigMap %s@%s.", cur.Name, cur.Namespace)
+	return c.CoreV1().ConfigMaps(cur.Namespace).Patch(cur.Name, types.JSONPatchType, pb)
+}
+
+func TryPatchConfigMap(c clientset.Interface, meta metav1.ObjectMeta, transform func(*apiv1.ConfigMap) *apiv1.ConfigMap) (*apiv1.ConfigMap, error) {
+	attempt := 0
+	for ; attempt < maxAttempts; attempt = attempt + 1 {
+		cur, err := c.CoreV1().ConfigMaps(meta.Namespace).Get(meta.Name, metav1.GetOptions{})
+		if kerr.IsNotFound(err) {
+			return cur, err
+		} else if err == nil {
+			return PatchConfigMap(c, cur, transform)
+		}
+		glog.Errorf("Attempt %d failed to patch ConfigMap %s@%s due to %s.", attempt, cur.Name, cur.Namespace, err)
+		time.Sleep(retryInterval)
+	}
+	return nil, fmt.Errorf("Failed to patch ConfigMap %s@%s after %d attempts.", meta.Name, meta.Namespace, attempt)
+}
+
+func UpdateConfigMap(c clientset.Interface, meta metav1.ObjectMeta, transform func(*apiv1.ConfigMap) *apiv1.ConfigMap) (*apiv1.ConfigMap, error) {
+	attempt := 0
+	for ; attempt < maxAttempts; attempt = attempt + 1 {
+		cur, err := c.CoreV1().ConfigMaps(meta.Namespace).Get(meta.Name, metav1.GetOptions{})
+		if kerr.IsNotFound(err) {
+			return cur, err
+		} else if err == nil {
+			return c.CoreV1().ConfigMaps(cur.Namespace).Update(transform(cur))
+		}
+		glog.Errorf("Attempt %d failed to update ConfigMap %s@%s due to %s.", attempt, cur.Name, cur.Namespace, err)
+		time.Sleep(retryInterval)
+	}
+	return nil, fmt.Errorf("Failed to update ConfigMap %s@%s after %d attempts.", meta.Name, meta.Namespace, attempt)
+}
