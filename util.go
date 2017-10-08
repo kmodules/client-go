f20137c6e3903fc4fa1e3ed76189bb37bff861a7
tamal <tamal@appscode.com>
2017-10-08 10:53:03 -0700

    Marshal/unmarshal using proper technique
    ref:
    https://github.com/kubernetes/kubeadm/issues/453

--- util.go
+++ util.go
@@ -9,6 +9,9 @@ import (
 	"strings"
 	"time"
 
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	clientsetscheme "k8s.io/client-go/kubernetes/scheme"
 	"github.com/hashicorp/go-version"
 	"github.com/pkg/errors"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
@@ -114,4 +117,28 @@ func GetKind(v interface{}) string {
 		val = val.Elem()
 	}
 	return val.Type().Name()
+}
+
+// MarshalToYAML marshals an object into yaml.
+func MarshalToYAML(obj runtime.Object, gv schema.GroupVersion) ([]byte, error) {
+	mediaType := "application/yaml"
+	info, ok := runtime.SerializerInfoForMediaType(clientsetscheme.Codecs.SupportedMediaTypes(), mediaType)
+	if !ok {
+		return []byte{}, fmt.Errorf("unsupported media type %q", mediaType)
+	}
+
+	encoder := clientsetscheme.Codecs.EncoderForVersion(info.Serializer, gv)
+	return runtime.Encode(encoder, obj)
+}
+
+// MarshalToJson marshals an object into yaml.
+func MarshalToJson(obj runtime.Object, gv schema.GroupVersion) ([]byte, error) {
+	mediaType := "application/json"
+	info, ok := runtime.SerializerInfoForMediaType(clientsetscheme.Codecs.SupportedMediaTypes(), mediaType)
+	if !ok {
+		return []byte{}, fmt.Errorf("unsupported media type %q", mediaType)
+	}
+
+	encoder := clientsetscheme.Codecs.EncoderForVersion(info.Serializer, gv)
+	return runtime.Encode(encoder, obj)
 }
