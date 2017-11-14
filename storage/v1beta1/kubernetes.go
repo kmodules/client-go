b746d106206155344c370b79e0b4d2b15b18c22f
Tamal Saha <tamal@appscode.com>
2017-11-14 11:15:20 -0800

    Reorganize util.go (#27)

--- storage/v1beta1/kubernetes.go
+++ storage/v1beta1/kubernetes.go
@@ -2,27 +2,27 @@ package v1beta1
 
 import (
 	"errors"
-	"fmt"
-	"reflect"
 
-	"github.com/appscode/kutil"
+	"github.com/appscode/kutil/meta"
+	"github.com/kubernetes/apimachinery/pkg/conversion"
 	storagev1beta1 "k8s.io/api/storage/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func GetGroupVersionKind(v interface{}) schema.GroupVersionKind {
-	return storagev1beta1.SchemeGroupVersion.WithKind(kutil.GetKind(v))
+	return storagev1beta1.SchemeGroupVersion.WithKind(meta.GetKind(v))
 }
 
 func AssignTypeKind(v interface{}) error {
-	if reflect.ValueOf(v).Kind() != reflect.Ptr {
-		return fmt.Errorf("%v must be a pointer", v)
+	_, err := conversion.EnforcePtr(v)
+	if err != nil {
+		return err
 	}
 
 	switch u := v.(type) {
 	case *storagev1beta1.StorageClass:
 		u.APIVersion = storagev1beta1.SchemeGroupVersion.String()
-		u.Kind = kutil.GetKind(v)
+		u.Kind = meta.GetKind(v)
 		return nil
 	}
 	return errors.New("unknown api object type")
