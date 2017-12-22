04124fd21c654188d7a11908d8c8af994a4cccef
Tamal Saha <tamal@appscode.com>
2017-12-22 03:01:59 -0500

    Add verb type updated & deleted

--- util.go
+++ util.go
@@ -14,4 +14,6 @@ const (
 	VerbUnchanged VerbType = ""
 	VerbCreated   VerbType = "created"
 	VerbPatched   VerbType = "patched"
+	VerbUpdated   VerbType = "updated"
+	VerbDeleted   VerbType = "deleted"
 )
