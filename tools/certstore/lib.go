efe12f4815eb2edcfac2b1b81f6b70214f85f54e
Tamal Saha <tamal@appscode.com>
2017-12-29 08:10:41 -0500

    Use github.com/spf13/afero as FileSystem abstraction (#51)

--- tools/certstore/lib.go
+++ tools/certstore/lib.go
@@ -4,17 +4,17 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"fmt"
-	"io/ioutil"
 	"net"
-	"os"
 	"path/filepath"
 	"strings"
 
 	netz "github.com/appscode/go/net"
+	"github.com/spf13/afero"
 	"k8s.io/client-go/util/cert"
 )
 
 type CertStore struct {
+	fs           afero.Fs
 	dir          string
 	organization []string
 
@@ -22,8 +22,8 @@ type CertStore struct {
 	caCert *x509.Certificate
 }
 
-func NewCertStore(dir string, organization ...string) (*CertStore, error) {
-	err := os.MkdirAll(dir, 0755)
+func NewCertStore(fs afero.Fs, dir string, organization ...string) (*CertStore, error) {
+	err := fs.MkdirAll(dir, 0755)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create dir `%s`. Reason: %v", dir, err)
 	}
@@ -125,18 +125,18 @@ func (cs *CertStore) NewClientCertPair(cn string, organization ...string) ([]byt
 }
 
 func (cs *CertStore) IsExists(name string) bool {
-	if _, err := os.Stat(cs.CertFile(name)); err == nil {
+	if _, err := cs.fs.Stat(cs.CertFile(name)); err == nil {
 		return true
 	}
-	if _, err := os.Stat(cs.KeyFile(name)); err == nil {
+	if _, err := cs.fs.Stat(cs.KeyFile(name)); err == nil {
 		return true
 	}
 	return false
 }
 
 func (cs *CertStore) PairExists(name string) bool {
-	if _, err := os.Stat(cs.CertFile(name)); err == nil {
-		if _, err := os.Stat(cs.KeyFile(name)); err == nil {
+	if _, err := cs.fs.Stat(cs.CertFile(name)); err == nil {
+		if _, err := cs.fs.Stat(cs.KeyFile(name)); err == nil {
 			return true
 		}
 	}
@@ -152,27 +152,27 @@ func (cs *CertStore) KeyFile(name string) string {
 }
 
 func (cs *CertStore) Write(name string, crt *x509.Certificate, key *rsa.PrivateKey) error {
-	if err := ioutil.WriteFile(cs.CertFile(name), cert.EncodeCertPEM(crt), 0644); err != nil {
+	if err := afero.WriteFile(cs.fs, cs.CertFile(name), cert.EncodeCertPEM(crt), 0644); err != nil {
 		return fmt.Errorf("failed to write `%cm`. Reason: %v", cs.CertFile(name), err)
 	}
-	if err := ioutil.WriteFile(cs.KeyFile(name), cert.EncodePrivateKeyPEM(key), 0600); err != nil {
+	if err := afero.WriteFile(cs.fs, cs.KeyFile(name), cert.EncodePrivateKeyPEM(key), 0600); err != nil {
 		return fmt.Errorf("failed to write `%cm`. Reason: %v", cs.KeyFile(name), err)
 	}
 	return nil
 }
 
 func (cs *CertStore) WriteBytes(name string, crt, key []byte) error {
-	if err := ioutil.WriteFile(cs.CertFile(name), crt, 0644); err != nil {
+	if err := afero.WriteFile(cs.fs, cs.CertFile(name), crt, 0644); err != nil {
 		return fmt.Errorf("failed to write `%cm`. Reason: %v", cs.CertFile(name), err)
 	}
-	if err := ioutil.WriteFile(cs.KeyFile(name), key, 0600); err != nil {
+	if err := afero.WriteFile(cs.fs, cs.KeyFile(name), key, 0600); err != nil {
 		return fmt.Errorf("failed to write `%cm`. Reason: %v", cs.KeyFile(name), err)
 	}
 	return nil
 }
 
 func (cs *CertStore) Read(name string) (*x509.Certificate, *rsa.PrivateKey, error) {
-	crtBytes, err := ioutil.ReadFile(cs.CertFile(name))
+	crtBytes, err := afero.ReadFile(cs.fs, cs.CertFile(name))
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to read certificate `%cm`. Reason: %v", cs.CertFile(name), err)
 	}
@@ -181,7 +181,7 @@ func (cs *CertStore) Read(name string) (*x509.Certificate, *rsa.PrivateKey, erro
 		return nil, nil, fmt.Errorf("failed to parse certificate `%cm`. Reason: %v", cs.CertFile(name), err)
 	}
 
-	keyBytes, err := ioutil.ReadFile(cs.KeyFile(name))
+	keyBytes, err := afero.ReadFile(cs.fs, cs.KeyFile(name))
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to read private key `%cm`. Reason: %v", cs.KeyFile(name), err)
 	}
@@ -193,12 +193,12 @@ func (cs *CertStore) Read(name string) (*x509.Certificate, *rsa.PrivateKey, erro
 }
 
 func (cs *CertStore) ReadBytes(name string) ([]byte, []byte, error) {
-	crtBytes, err := ioutil.ReadFile(cs.CertFile(name))
+	crtBytes, err := afero.ReadFile(cs.fs, cs.CertFile(name))
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to read certificate `%cm`. Reason: %v", cs.CertFile(name), err)
 	}
 
-	keyBytes, err := ioutil.ReadFile(cs.KeyFile(name))
+	keyBytes, err := afero.ReadFile(cs.fs, cs.KeyFile(name))
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to read private key `%cm`. Reason: %v", cs.KeyFile(name), err)
 	}
