27d2cfe8446ebe2627406a43081ff7d0f222e8e7
tamal <tamal@appscode.com>
2018-03-20 03:52:48 -0700

    Keep selector in workload

--- workload/v1/types.go
+++ workload/v1/types.go
@@ -15,12 +15,23 @@ type Workload struct {
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
+	Spec WorkloadSpec `json:"spec,omitempty"`
+
+	Object runtime.Object `json:"-"`
+}
+
+// WorkloadSpec is the specification of a workload.
+type WorkloadSpec struct {
+	// A label query over pods that are managed by the daemon set.
+	// Must match in order to be controlled.
+	// It must match the pod template's labels.
+	// More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#label-selectors
+	Selector *metav1.LabelSelector `json:"selector"`
+
 	// An object that describes the pod that will be created.
 	// The DaemonSet will create exactly one copy of this pod on every node
 	// that matches the template's node selector (or on every node if no node
 	// selector is specified).
 	// More info: https://kubernetes.io/docs/concepts/workloads/controllers/replicationcontroller#pod-template
-	Template core.PodTemplateSpec `json:"template" protobuf:"bytes,2,opt,name=template"`
-
-	Object runtime.Object `json:"-"`
+	Template core.PodTemplateSpec `json:"template"`
 }
