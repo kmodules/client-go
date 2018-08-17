6b6996278a6a2d2ea10f2a1c0189c573eef32131
Tamal Saha <tamal@appscode.com>
2018-08-16 23:06:12 -0700

    Add FilterKeys
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- meta/lib_test.go
+++ meta/lib_test.go
@@ -2,6 +2,7 @@ package meta_test
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/appscode/kutil/meta"
@@ -44,4 +45,127 @@ func TestMarshalToYAML(t *testing.T) {
 	b, err := meta.MarshalToYAML(obj, core.SchemeGroupVersion)
 	fmt.Println(err)
 	fmt.Println(string(b))
+}
+
+const domainKey = "kubedb.com"
+
+func TestFilterKeys(t *testing.T) {
+	cases := []struct {
+		name string
+		in   map[string]string
+		out  map[string]string
+	}{
+		{
+			"IndexRune < 0",
+			map[string]string{
+				"k": "v",
+			},
+			map[string]string{
+				"k": "v",
+			},
+		},
+		{
+			"IndexRune == 0",
+			map[string]string{
+				"/k": "v",
+			},
+			map[string]string{
+				"/k": "v",
+			},
+		},
+		{
+			"IndexRune < n - xyz.abc/w1",
+			map[string]string{
+				"xyz.abc/w1": "v1",
+				"w2":         "v2",
+			},
+			map[string]string{
+				"xyz.abc/w1": "v1",
+				"w2":         "v2",
+			},
+		},
+		{
+			"IndexRune < n - .abc/w1",
+			map[string]string{
+				".abc/w1": "v1",
+				"w2":      "v2",
+			},
+			map[string]string{
+				".abc/w1": "v1",
+				"w2":      "v2",
+			},
+		},
+		{
+			"IndexRune == n - matching_domain",
+			map[string]string{
+				domainKey + "/w1": "v1",
+				"w2":              "v2",
+			},
+			map[string]string{
+				"w2": "v2",
+			},
+		},
+		{
+			"IndexRune > n - matching_subdomain",
+			map[string]string{
+				"xyz." + domainKey + "/w1": "v1",
+				"w2": "v2",
+			},
+			map[string]string{
+				"w2": "v2",
+			},
+		},
+		{
+			"IndexRune > n - matching_subdomain-2",
+			map[string]string{
+				"." + domainKey + "/w1": "v1",
+				"w2": "v2",
+			},
+			map[string]string{
+				"w2": "v2",
+			},
+		},
+		{
+			"IndexRune == n - unmatched_domain",
+			map[string]string{
+				"cubedb.com/w1": "v1",
+				"w2":            "v2",
+			},
+			map[string]string{
+				"cubedb.com/w1": "v1",
+				"w2":            "v2",
+			},
+		},
+		{
+			"IndexRune > n - unmatched_subdomain",
+			map[string]string{
+				"xyz.cubedb.com/w1": "v1",
+				"w2":                "v2",
+			},
+			map[string]string{
+				"xyz.cubedb.com/w1": "v1",
+				"w2":                "v2",
+			},
+		},
+		{
+			"IndexRune > n - unmatched_subdomain-2",
+			map[string]string{
+				".cubedb.com/w1": "v1",
+				"w2":             "v2",
+			},
+			map[string]string{
+				".cubedb.com/w1": "v1",
+				"w2":             "v2",
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			result := meta.FilterKeys(domainKey, nil, c.in)
+			if !reflect.DeepEqual(c.out, result) {
+				t.Errorf("Failed filterTag test for '%v': expected %+v, got %+v", c.in, c.out, result)
+			}
+		})
+	}
 }
