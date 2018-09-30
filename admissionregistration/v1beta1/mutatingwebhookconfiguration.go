c7492974147153149ba8fcf00742fdb56bd6ae61
Tamal Saha <tamal@appscode.com>
2018-09-29 17:52:49 -0700

    Add (Update | Sync)UpdateMutatingWebhookCABundle helper methods (#245)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- admissionregistration/v1beta1/mutatingwebhookconfiguration.go
+++ admissionregistration/v1beta1/mutatingwebhookconfiguration.go
@@ -1,26 +1,36 @@
 package v1beta1
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/appscode/kutil"
+	watchtools "github.com/appscode/kutil/tools/watch"
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
-	apps "k8s.io/api/admissionregistration/v1beta1"
+	reg "k8s.io/api/admissionregistration/v1beta1"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 )
 
-func CreateOrPatchMutatingWebhookConfiguration(c kubernetes.Interface, name string, transform func(*apps.MutatingWebhookConfiguration) *apps.MutatingWebhookConfiguration) (*apps.MutatingWebhookConfiguration, kutil.VerbType, error) {
+func CreateOrPatchMutatingWebhookConfiguration(c kubernetes.Interface, name string, transform func(*reg.MutatingWebhookConfiguration) *reg.MutatingWebhookConfiguration) (*reg.MutatingWebhookConfiguration, kutil.VerbType, error) {
 	cur, err := c.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Get(name, metav1.GetOptions{})
 	if kerr.IsNotFound(err) {
 		glog.V(3).Infof("Creating MutatingWebhookConfiguration %s.", name)
-		out, err := c.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Create(transform(&apps.MutatingWebhookConfiguration{
+		out, err := c.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Create(transform(&reg.MutatingWebhookConfiguration{
 			TypeMeta: metav1.TypeMeta{
 				Kind:       "MutatingWebhookConfiguration",
-				APIVersion: apps.SchemeGroupVersion.String(),
+				APIVersion: reg.SchemeGroupVersion.String(),
 			},
 			ObjectMeta: metav1.ObjectMeta{
 				Name: name,
@@ -33,11 +43,11 @@ func CreateOrPatchMutatingWebhookConfiguration(c kubernetes.Interface, name stri
 	return PatchMutatingWebhookConfiguration(c, cur, transform)
 }
 
-func PatchMutatingWebhookConfiguration(c kubernetes.Interface, cur *apps.MutatingWebhookConfiguration, transform func(*apps.MutatingWebhookConfiguration) *apps.MutatingWebhookConfiguration) (*apps.MutatingWebhookConfiguration, kutil.VerbType, error) {
+func PatchMutatingWebhookConfiguration(c kubernetes.Interface, cur *reg.MutatingWebhookConfiguration, transform func(*reg.MutatingWebhookConfiguration) *reg.MutatingWebhookConfiguration) (*reg.MutatingWebhookConfiguration, kutil.VerbType, error) {
 	return PatchMutatingWebhookConfigurationObject(c, cur, transform(cur.DeepCopy()))
 }
 
-func PatchMutatingWebhookConfigurationObject(c kubernetes.Interface, cur, mod *apps.MutatingWebhookConfiguration) (*apps.MutatingWebhookConfiguration, kutil.VerbType, error) {
+func PatchMutatingWebhookConfigurationObject(c kubernetes.Interface, cur, mod *reg.MutatingWebhookConfiguration) (*reg.MutatingWebhookConfiguration, kutil.VerbType, error) {
 	curJson, err := json.Marshal(cur)
 	if err != nil {
 		return nil, kutil.VerbUnchanged, err
@@ -48,7 +58,7 @@ func PatchMutatingWebhookConfigurationObject(c kubernetes.Interface, cur, mod *a
 		return nil, kutil.VerbUnchanged, err
 	}
 
-	patch, err := strategicpatch.CreateTwoWayMergePatch(curJson, modJson, apps.MutatingWebhookConfiguration{})
+	patch, err := strategicpatch.CreateTwoWayMergePatch(curJson, modJson, reg.MutatingWebhookConfiguration{})
 	if err != nil {
 		return nil, kutil.VerbUnchanged, err
 	}
@@ -60,7 +70,7 @@ func PatchMutatingWebhookConfigurationObject(c kubernetes.Interface, cur, mod *a
 	return out, kutil.VerbPatched, err
 }
 
-func TryUpdateMutatingWebhookConfiguration(c kubernetes.Interface, name string, transform func(*apps.MutatingWebhookConfiguration) *apps.MutatingWebhookConfiguration) (result *apps.MutatingWebhookConfiguration, err error) {
+func TryUpdateMutatingWebhookConfiguration(c kubernetes.Interface, name string, transform func(*reg.MutatingWebhookConfiguration) *reg.MutatingWebhookConfiguration) (result *reg.MutatingWebhookConfiguration, err error) {
 	attempt := 0
 	err = wait.PollImmediate(kutil.RetryInterval, kutil.RetryTimeout, func() (bool, error) {
 		attempt++
@@ -78,5 +88,105 @@ func TryUpdateMutatingWebhookConfiguration(c kubernetes.Interface, name string, 
 	if err != nil {
 		err = errors.Errorf("failed to update MutatingWebhookConfiguration %s after %d attempts due to %v", name, attempt, err)
 	}
+	return
+}
+
+func UpdateMutatingWebhookCABundle(config *rest.Config, name string) error {
+	err := rest.LoadTLSFiles(config)
+	if err != nil {
+		return err
+	}
+
+	kc := kubernetes.NewForConfigOrDie(config)
+
+	lw := &cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			options.FieldSelector = fields.OneTermEqualSelector(kutil.ObjectNameField, name).String()
+			return kc.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().List(options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			options.FieldSelector = fields.OneTermEqualSelector(kutil.ObjectNameField, name).String()
+			return kc.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Watch(options)
+		},
+	}
+
+	ctx := context.Background()
+	_, err = watchtools.UntilWithSync(ctx,
+		lw,
+		&reg.MutatingWebhookConfiguration{},
+		nil,
+		func(event watch.Event) (bool, error) {
+			a, _ := meta.Accessor(event.Object)
+			fmt.Println(event.Type, a.GetName())
+
+			switch event.Type {
+			case watch.Deleted:
+				return false, nil
+			case watch.Error:
+				return false, errors.Wrap(err, "error watching")
+			case watch.Added, watch.Modified:
+				cur := event.Object.(*reg.MutatingWebhookConfiguration)
+				_, _, err := PatchMutatingWebhookConfiguration(kc, cur, func(in *reg.MutatingWebhookConfiguration) *reg.MutatingWebhookConfiguration {
+					for i := range in.Webhooks {
+						in.Webhooks[i].ClientConfig.CABundle = config.CAData
+					}
+					return in
+				})
+				return err == nil, err
+			default:
+				return false, fmt.Errorf("unexpected event type: %v", event.Type)
+			}
+		})
+	return err
+}
+
+func SyncMutatingWebhookCABundle(config *rest.Config, name string) (cancel context.CancelFunc, err error) {
+	err = rest.LoadTLSFiles(config)
+	if err != nil {
+		return
+	}
+
+	kc := kubernetes.NewForConfigOrDie(config)
+
+	lw := &cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			options.FieldSelector = fields.OneTermEqualSelector(kutil.ObjectNameField, name).String()
+			return kc.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().List(options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			options.FieldSelector = fields.OneTermEqualSelector(kutil.ObjectNameField, name).String()
+			return kc.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Watch(options)
+		},
+	}
+
+	ctx := context.Background()
+	ctx, cancel = context.WithCancel(ctx)
+
+	go watchtools.UntilWithSync(ctx,
+		lw,
+		&reg.MutatingWebhookConfiguration{},
+		nil,
+		func(event watch.Event) (bool, error) {
+			a, _ := meta.Accessor(event.Object)
+			fmt.Println(event.Type, a.GetName())
+
+			switch event.Type {
+			case watch.Deleted:
+				return false, nil
+			case watch.Error:
+				return false, errors.Wrap(err, "error watching")
+			case watch.Added, watch.Modified:
+				cur := event.Object.(*reg.MutatingWebhookConfiguration)
+				_, _, err := PatchMutatingWebhookConfiguration(kc, cur, func(in *reg.MutatingWebhookConfiguration) *reg.MutatingWebhookConfiguration {
+					for i := range in.Webhooks {
+						in.Webhooks[i].ClientConfig.CABundle = config.CAData
+					}
+					return in
+				})
+				return false, err // continue
+			default:
+				return false, fmt.Errorf("unexpected event type: %v", event.Type)
+			}
+		})
 	return
 }
