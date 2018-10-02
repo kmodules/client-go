b38804c5c1fb055e9700b9ae832578aade0d305b
Tamal Saha <tamal@appscode.com>
2018-10-02 09:38:06 -0700

    Make FixAKS public
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- tools/clientcmd/client_config.go
+++ tools/clientcmd/client_config.go
@@ -18,13 +18,13 @@ import (
 // are passed in we fallback to inClusterConfig. If inClusterConfig fails, we fallback
 // to the default config.
 func BuildConfigFromFlags(masterUrl, kubeconfigPath string) (*rest.Config, error) {
-	return fixAKS(clientcmd.BuildConfigFromFlags(masterUrl, kubeconfigPath))
+	return fix(clientcmd.BuildConfigFromFlags(masterUrl, kubeconfigPath))
 }
 
 // BuildConfigFromKubeconfigGetter is a helper function that builds configs from a master
 // url and a kubeconfigGetter.
 func BuildConfigFromKubeconfigGetter(masterUrl string, kubeconfigGetter clientcmd.KubeconfigGetter) (*rest.Config, error) {
-	return fixAKS(clientcmd.BuildConfigFromKubeconfigGetter(masterUrl, kubeconfigGetter))
+	return fix(clientcmd.BuildConfigFromKubeconfigGetter(masterUrl, kubeconfigGetter))
 }
 
 func BuildConfigFromContext(kubeconfigPath, contextName string) (*rest.Config, error) {
@@ -42,7 +42,7 @@ func BuildConfigFromContext(kubeconfigPath, contextName string) (*rest.Config, e
 	overrides := &clientcmd.ConfigOverrides{
 		CurrentContext: contextName,
 	}
-	return fixAKS(clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loader, overrides).ClientConfig())
+	return fix(clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loader, overrides).ClientConfig())
 }
 
 func ClientFromContext(kubeconfigPath, contextName string) (kubernetes.Interface, error) {
@@ -65,20 +65,24 @@ func NamespaceFromContext(kubeconfigPath, contextName string) (string, error) {
 	return ctx.Namespace, nil
 }
 
-func fixAKS(cfg *rest.Config, err error) (*rest.Config, error) {
-	if err != nil {
-		return cfg, err
+func fix(cfg *rest.Config, err error) (*rest.Config, error) {
+	return FixAKS(cfg), err
+}
+
+func FixAKS(cfg *rest.Config) *rest.Config {
+	if cfg == nil {
+		return nil
 	}
 
 	// ref: https://github.com/kubernetes/client-go/blob/kubernetes-1.11.3/rest/config.go#L309
 	host, port := os.Getenv("KUBERNETES_SERVICE_HOST"), os.Getenv("KUBERNETES_SERVICE_PORT")
 	if len(host) == 0 || len(port) == 0 {
-		return cfg, err
+		return cfg
 	}
 	if cfg.Host != "https://"+net.JoinHostPort(host, port) &&
 		cfg.Host != "https://kubernetes.default.svc" &&
 		cfg.Host != "https://kubernetes.default.svc:443" {
-		return cfg, err
+		return cfg
 	}
 
 	if cert, err := meta.APIServerCertificate(cfg); err == nil {
@@ -86,5 +90,5 @@ func fixAKS(cfg *rest.Config, err error) (*rest.Config, error) {
 			cfg.Host = "https://" + host
 		}
 	}
-	return cfg, nil
+	return cfg
 }
