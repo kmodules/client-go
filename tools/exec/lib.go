b5b6fa2ff0180040757643f9a498ea84d7bd8dd6
Tamal Saha <tamal@appscode.com>
2019-02-01 10:19:11 -0800

    Support functional options for exec tools (#295)
    xref: https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- tools/exec/lib.go
+++ tools/exec/lib.go
@@ -3,6 +3,8 @@ package exec
 import (
 	"bytes"
 	"fmt"
+	"net/http"
+	"strings"
 
 	core "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
@@ -11,11 +13,56 @@ import (
 	"k8s.io/client-go/tools/remotecommand"
 )
 
-func ExecIntoPod(config *rest.Config, pod *core.Pod, command ...string) (string, error) {
+type Options struct {
+	core.PodExecOptions
+	remotecommand.StreamOptions
+}
+
+func Container(container string) func(*Options) {
+	return func(opts *Options) {
+		opts.Container = container
+	}
+}
+
+func Command(cmd ...string) func(*Options) {
+	return func(opts *Options) {
+		opts.Command = cmd
+	}
+}
+
+func Input(in string) func(*Options) {
+	return func(opts *Options) {
+		opts.PodExecOptions.Stdin = true
+		opts.StreamOptions.Stdin = strings.NewReader(in)
+	}
+}
+
+func TTY(enable bool) func(*Options) {
+	return func(opts *Options) {
+		opts.PodExecOptions.TTY = enable
+	}
+}
+
+func ExecIntoPod(config *rest.Config, pod *core.Pod, options ...func(*Options)) (string, error) {
 	var (
 		execOut bytes.Buffer
 		execErr bytes.Buffer
+		opts    = &Options{
+			PodExecOptions: core.PodExecOptions{
+				Container: pod.Spec.Containers[0].Name,
+				Stdout:    true,
+				Stderr:    true,
+			},
+			StreamOptions: remotecommand.StreamOptions{
+				Stdout: &execOut,
+				Stderr: &execErr,
+			},
+		}
 	)
+	for _, option := range options {
+		option(opts)
+	}
+
 	kc := kubernetes.NewForConfigOrDie(config)
 
 	req := kc.CoreV1().RESTClient().Post().
@@ -23,22 +70,14 @@ func ExecIntoPod(config *rest.Config, pod *core.Pod, command ...string) (string,
 		Name(pod.Name).
 		Namespace(pod.Namespace).
 		SubResource("exec")
-	req.VersionedParams(&core.PodExecOptions{
-		Container: pod.Spec.Containers[0].Name,
-		Command:   command,
-		Stdout:    true,
-		Stderr:    true,
-	}, scheme.ParameterCodec)
+	req.VersionedParams(&opts.PodExecOptions, scheme.ParameterCodec)
 
-	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	exec, err := remotecommand.NewSPDYExecutor(config, http.MethodPost, req.URL())
 	if err != nil {
 		return "", fmt.Errorf("failed to init executor: %v", err)
 	}
 
-	err = exec.Stream(remotecommand.StreamOptions{
-		Stdout: &execOut,
-		Stderr: &execErr,
-	})
+	err = exec.Stream(opts.StreamOptions)
 
 	if err != nil {
 		return "", fmt.Errorf("could not execute: %v", err)
