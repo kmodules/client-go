3c3af4833705eb6a9adef8e7184f414e9a519f2c
Tamal Saha <tamal@appscode.com>
2020-01-25 12:47:12 -0800

    Add labels to DetectTopology
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- core/v1/node.go
+++ core/v1/node.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
+	"gomodules.xyz/version"
 	core "k8s.io/api/core/v1"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -121,11 +122,55 @@ func IsMaster(node core.Node) bool {
 	return ok17 || (ok16 && role16 == "master")
 }
 
-func Topology(kc kubernetes.Interface) (regions map[string][]string, instances map[string]int, err error) {
+type Zones []string
+
+type Topology struct {
+	Regions           map[string][]string
+	TotalNodes        int
+	InstanceTypes     map[string]int
+	LabelZone         string
+	LabelRegion       string
+	LabelInstanceType string
+
+	// https://github.com/kubernetes/kubernetes/blob/v1.17.2/staging/src/k8s.io/api/core/v1/well_known_labels.go
+
+	//LabelHostname = "kubernetes.io/hostname"
+	//
+	//LabelZoneFailureDomain       = "failure-domain.beta.kubernetes.io/zone"
+	//LabelZoneRegion              = "failure-domain.beta.kubernetes.io/region"
+	//LabelZoneFailureDomainStable = "topology.kubernetes.io/zone"
+	//LabelZoneRegionStable        = "topology.kubernetes.io/region"
+	//
+	//LabelInstanceType       = "beta.kubernetes.io/instance-type"
+	//LabelInstanceTypeStable = "node.kubernetes.io/instance-type"
+}
+
+func DetectTopology(kc kubernetes.Interface) (*Topology, error) {
 	// TODO: Use https://github.com/kubernetes/client-go/blob/kubernetes-1.17.0/metadata/interface.go once upgraded to 1.17
 
+	var topology Topology
+
+	info, err := kc.Discovery().ServerVersion()
+	if err != nil {
+		return nil, err
+	}
+	ver, err := version.NewVersion(info.GitVersion)
+	if err != nil {
+		return nil, err
+	}
+	ver = ver.ToMutator().ResetPrerelease().ResetMetadata().Done()
+	if ver.Major() >= 1 && ver.Minor() >= 17 {
+		topology.LabelZone = "topology.kubernetes.io/zone"
+		topology.LabelRegion = "topology.kubernetes.io/region"
+		topology.LabelInstanceType = "node.kubernetes.io/instance-type"
+	} else {
+		topology.LabelZone = core.LabelZoneFailureDomain
+		topology.LabelRegion = core.LabelZoneRegion
+		topology.LabelInstanceType = core.LabelInstanceType
+	}
+
 	mapRegion := make(map[string]sets.String)
-	instances = make(map[string]int)
+	instances := make(map[string]int)
 
 	lister := pager.New(pager.SimplePageFunc(func(opts metav1.ListOptions) (runtime.Object, error) {
 		return kc.CoreV1().Nodes().List(opts)
@@ -164,12 +209,15 @@ func Topology(kc kubernetes.Interface) (regions map[string][]string, instances m
 		return nil
 	})
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	regions = make(map[string][]string)
+	regions := make(map[string][]string)
 	for k, v := range mapRegion {
 		regions[k] = v.List()
 	}
-	return regions, instances, nil
+	topology.Regions = regions
+	topology.InstanceTypes = instances
+
+	return &topology, nil
 }
