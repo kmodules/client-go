bb3257d562df2d3094e40ae9daae2155519407c0
Tamal Saha <tamal@appscode.com>
2021-04-15 01:58:10 -0700

    Add IPv6Enabled (#405)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ meta/ipv6.go
@@ -0,0 +1,26 @@
+package meta
+
+import (
+	"context"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func IPv6Enabled(kc kubernetes.Interface) (bool, error) {
+	svc, err := kc.CoreV1().Services(metav1.NamespaceDefault).Get(context.TODO(), "kubernetes", metav1.GetOptions{})
+	if err != nil {
+		return false, err
+	}
+	clusterIPs := svc.Spec.ClusterIPs
+	if len(clusterIPs) == 0 {
+		clusterIPs = []string{svc.Spec.ClusterIP}
+	}
+	for _, ip := range clusterIPs {
+		if strings.ContainsRune(ip, ':') {
+			return true, nil
+		}
+	}
+	return false, nil
+}
