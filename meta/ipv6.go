f27f7a411d5c4a21b9dc0b423ab8c77db60eef4f
Mohammad Fahim Abrar <fahimabrar@appscode.com>
2021-04-16 14:43:29 +0600

    Add `IPv6EnabledInKernel` (#406)
    Signed-off-by: Mohammad Fahim Abrar <fahimabrar@appscode.com>

--- meta/ipv6.go
+++ meta/ipv6.go
@@ -2,13 +2,14 @@ package meta
 
 import (
 	"context"
+	"io/ioutil"
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-func IPv6Enabled(kc kubernetes.Interface) (bool, error) {
+func IPv6EnabledInCluster(kc kubernetes.Interface) (bool, error) {
 	svc, err := kc.CoreV1().Services(metav1.NamespaceDefault).Get(context.TODO(), "kubernetes", metav1.GetOptions{})
 	if err != nil {
 		return false, err
@@ -20,4 +21,12 @@ func IPv6Enabled(kc kubernetes.Interface) (bool, error) {
 		}
 	}
 	return false, nil
+}
+
+func IPv6EnabledInKernel() (bool, error) {
+	content, err := ioutil.ReadFile("/sys/module/ipv6/parameters/disable")
+	if err != nil {
+		return false, err
+	}
+	return strings.TrimSpace(string(content)) == "0", nil
 }
