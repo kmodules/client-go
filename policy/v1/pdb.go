e0527e2ea221f7f8b674c2f7b4e4df24a0e3a8f4
Tamal Saha <tamal@appscode.com>
2022-08-25 11:14:29 -0700

    Remove utils for apis removed in Kubernetes 1.25
    xref: https://kubernetes.io/docs/reference/using-api/deprecation-guide/#v1-25
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- policy/v1/pdb.go
+++ policy/v1/pdb.go
@@ -22,7 +22,7 @@ import (
 	"kmodules.xyz/client-go/discovery"
 
 	"github.com/pkg/errors"
-	policy "k8s.io/api/policy/v1beta1"
+	policy "k8s.io/api/policy/v1"
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -35,10 +35,10 @@ import (
 )
 
 func CreateOrPatchPodDisruptionBudget(ctx context.Context, c kubernetes.Interface, meta metav1.ObjectMeta, transform func(*policy.PodDisruptionBudget) *policy.PodDisruptionBudget, opts metav1.PatchOptions) (*policy.PodDisruptionBudget, kutil.VerbType, error) {
-	cur, err := c.PolicyV1beta1().PodDisruptionBudgets(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
+	cur, err := c.PolicyV1().PodDisruptionBudgets(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
 	if kerr.IsNotFound(err) {
 		klog.V(3).Infof("Creating PodDisruptionBudget %s/%s.", meta.Namespace, meta.Name)
-		out, err := c.PolicyV1beta1().PodDisruptionBudgets(meta.Namespace).Create(ctx, transform(&policy.PodDisruptionBudget{
+		out, err := c.PolicyV1().PodDisruptionBudgets(meta.Namespace).Create(ctx, transform(&policy.PodDisruptionBudget{
 			TypeMeta: metav1.TypeMeta{
 				Kind:       "PodDisruptionBudget",
 				APIVersion: policy.SchemeGroupVersion.String(),
@@ -61,12 +61,12 @@ func CreateOrPatchPodDisruptionBudget(ctx context.Context, c kubernetes.Interfac
 		}
 		// PDBs dont have the specs, Specs can't be modified once created, so we have to delete first, then recreate with correct  spec
 		klog.Warningf("Spec of PodDisruptionBudget %s/%s is modified, deleting existing one first.", meta.Namespace, meta.Name)
-		err = c.PolicyV1beta1().PodDisruptionBudgets(meta.Namespace).Delete(ctx, meta.Name, metav1.DeleteOptions{})
+		err = c.PolicyV1().PodDisruptionBudgets(meta.Namespace).Delete(ctx, meta.Name, metav1.DeleteOptions{})
 		if err != nil {
 			return nil, kutil.VerbUnchanged, err
 		}
 		klog.V(3).Infof("Creating PodDisruptionBudget %s/%s.", mod.Namespace, mod.Name)
-		out, err := c.PolicyV1beta1().PodDisruptionBudgets(meta.Namespace).Create(ctx, transform(&policy.PodDisruptionBudget{
+		out, err := c.PolicyV1().PodDisruptionBudgets(meta.Namespace).Create(ctx, transform(&policy.PodDisruptionBudget{
 			TypeMeta: metav1.TypeMeta{
 				Kind:       "PodDisruptionBudget",
 				APIVersion: policy.SchemeGroupVersion.String(),
@@ -107,7 +107,7 @@ func PatchPodDisruptionBudgetObject(ctx context.Context, c kubernetes.Interface,
 		return cur, kutil.VerbUnchanged, nil
 	}
 	klog.V(3).Infof("Patching PodDisruptionBudget %s with %s.", cur.Name, string(patch))
-	out, err := c.PolicyV1beta1().PodDisruptionBudgets(cur.Namespace).Patch(ctx, cur.Name, types.StrategicMergePatchType, patch, opts)
+	out, err := c.PolicyV1().PodDisruptionBudgets(cur.Namespace).Patch(ctx, cur.Name, types.StrategicMergePatchType, patch, opts)
 	return out, kutil.VerbPatched, err
 }
 
@@ -115,11 +115,11 @@ func TryUpdatePodDisruptionBudget(ctx context.Context, c kubernetes.Interface, m
 	attempt := 0
 	err = wait.PollImmediate(kutil.RetryInterval, kutil.RetryTimeout, func() (bool, error) {
 		attempt++
-		cur, e2 := c.PolicyV1beta1().PodDisruptionBudgets(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
+		cur, e2 := c.PolicyV1().PodDisruptionBudgets(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
 		if kerr.IsNotFound(e2) {
 			return false, e2
 		} else if e2 == nil {
-			result, e2 = c.PolicyV1beta1().PodDisruptionBudgets(meta.Namespace).Update(ctx, transform(cur.DeepCopy()), opts)
+			result, e2 = c.PolicyV1().PodDisruptionBudgets(meta.Namespace).Update(ctx, transform(cur.DeepCopy()), opts)
 			return e2 == nil, nil
 		}
 		klog.Errorf("Attempt %d failed to update PodDisruptionBudget %s due to %v.", attempt, cur.Name, e2)
