2bd8fd6efdd4fe8f40c379a973178a5b24fdf796
Tamal Saha <tamal@appscode.com>
2022-12-20 15:51:27 +0600

    Fix Eviction version detection
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- policy/evict.go
+++ policy/evict.go
@@ -18,17 +18,46 @@ package policy
 
 import (
 	"context"
+	"strconv"
 
+	"gomodules.xyz/sync"
 	policyv1 "k8s.io/api/policy/v1"
 	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/kubernetes"
 )
 
+var (
+	onceEviction  sync.Once
+	useEvictionV1 bool
+)
+
+func detectEvictionVersion(c discovery.DiscoveryInterface) {
+	onceEviction.Do(func() error {
+		// Note: policy/v1 Eviction is available in v1.22+. Use policy/v1beta1 with prior releases.
+		// ref: https://kubernetes.io/docs/concepts/scheduling-eviction/api-eviction/#calling-the-eviction-api
+		info, err := c.ServerVersion()
+		if err != nil {
+			return err
+		}
+		major, err := strconv.Atoi(info.Major)
+		if err != nil {
+			return err
+		}
+		minor, err := strconv.Atoi(info.Minor)
+		if err != nil {
+			return err
+		}
+		useEvictionV1 = major > 1 || (major == 1 && minor >= 22)
+		return err
+	})
+}
+
 func EvictPod(ctx context.Context, c kubernetes.Interface, meta types.NamespacedName, opts *metav1.DeleteOptions) error {
-	detectVersion(c.Discovery())
-	if useV1 {
+	detectEvictionVersion(c.Discovery())
+	if useEvictionV1 {
 		return c.CoreV1().Pods(meta.Namespace).EvictV1(ctx, &policyv1.Eviction{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      meta.Name,
