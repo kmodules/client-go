22c7047f48aeb8018984e12bafea4af11be8964d
Tamal Saha <tamal@appscode.com>
2024-06-20 04:59:45 -0700

    Support multi crd registration support
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apiextensions/controller.go
+++ apiextensions/controller.go
@@ -29,15 +29,20 @@ import (
 
 type (
 	SetupFn func(ctx context.Context, mgr ctrl.Manager)
-	TestFn  func(*apiextensionsv1.CustomResourceDefinition) (bool, any)
+	TestFn  func(*apiextensionsv1.CustomResourceDefinition) bool
 )
 
+type setupGroup struct {
+	gks []schema.GroupKind
+	fn  SetupFn
+}
+
 var (
-	setupFns        = make(map[schema.GroupKind]SetupFn)
-	testFns         = make(map[schema.GroupKind]TestFn)
-	setupDone       = map[schema.GroupKind]bool{}
-	ExtraSetupParam = struct{}{}
-	mu              sync.Mutex
+	setupFns  = make(map[schema.GroupKind]setupGroup)
+	testFns   = make(map[schema.GroupKind]TestFn)
+	setupDone = map[schema.GroupKind]bool{}
+	CRDParam  = struct{}{}
+	mu        sync.Mutex
 )
 
 type Reconciler struct {
@@ -72,18 +77,16 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	if !setupFnExists {
 		return ctrl.Result{}, nil
 	}
-
-	ctxSetup := r.ctx
-	if test, testFnExists := testFns[gk]; testFnExists {
-		passed, extra := test(&crd)
-		if !passed {
-			return ctrl.Result{}, nil
-		}
-		ctxSetup = context.WithValue(r.ctx, ExtraSetupParam, extra)
+	if !testFns[gk](&crd) {
+		return ctrl.Result{}, nil
 	}
 
-	setup(ctxSetup, r.mgr)
-	setupDone[gk] = true
+	ctxSetup := context.WithValue(r.ctx, CRDParam, &crd)
+	setup.fn(ctxSetup, r.mgr)
+
+	for _, gk := range setup.gks {
+		setupDone[gk] = true
+	}
 	return ctrl.Result{}, nil
 }
 
@@ -97,8 +100,34 @@ func RegisterSetup(gk schema.GroupKind, fn SetupFn, tn ...TestFn) {
 	mu.Lock()
 	defer mu.Unlock()
 
-	setupFns[gk] = fn
-	if len(tn) == 1 {
-		testFns[gk] = tn[0]
+	setupFns[gk] = setupGroup{
+		gks: []schema.GroupKind{gk},
+		fn:  fn,
+	}
+	testFns[gk] = andTestFn(tn...)
+}
+
+func MultiRegisterSetup(gks []schema.GroupKind, fn SetupFn, tn ...TestFn) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	testFN := andTestFn(tn...)
+	for _, gk := range gks {
+		setupFns[gk] = setupGroup{
+			gks: gks,
+			fn:  fn,
+		}
+		testFns[gk] = testFN
+	}
+}
+
+func andTestFn(fns ...TestFn) TestFn {
+	return func(crd *apiextensionsv1.CustomResourceDefinition) bool {
+		for _, fn := range fns {
+			if !fn(crd) {
+				return false
+			}
+		}
+		return true
 	}
 }
