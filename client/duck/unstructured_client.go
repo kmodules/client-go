9d78539e87eb2edbff958c48b72adee796d1b3d1
Arnob kumar saha <arnob@appscode.com>
2024-10-23 16:06:05 +0600

    Patch status with duck client
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- client/duck/unstructured_client.go
+++ client/duck/unstructured_client.go
@@ -258,12 +258,17 @@ func (sw *unstructuredStatusWriter) Patch(ctx context.Context, obj client.Object
 		return sw.client.c.Status().Patch(ctx, obj, patch, opts...)
 	}
 
+	rawPatch, err := NewRawPatch(obj, patch)
+	if err != nil {
+		return err
+	}
+
 	var llo unstructured.Unstructured
 	llo.GetObjectKind().SetGroupVersionKind(sw.client.rawGVK)
 	llo.SetNamespace(obj.GetNamespace())
 	llo.SetName(obj.GetName())
 	llo.SetLabels(obj.GetLabels())
-	return sw.client.c.Status().Patch(ctx, &llo, patch, opts...)
+	return sw.client.c.Status().Patch(ctx, &llo, rawPatch, opts...)
 }
 
 func (d *unstructuredClient) SubResource(subResource string) client.SubResourceClient {
